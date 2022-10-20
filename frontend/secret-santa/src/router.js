cad885691cf56ce34eaba5781b04e9985d421340
Kevin Chen <kcchen33@gmail.com>
2022-10-20 22:03:16 +0200

    add exploding santa when opting out

--- frontend/secret-santa/src/router.js
+++ frontend/secret-santa/src/router.js
@@ -3,12 +3,14 @@ import { useRoutes } from "react-router-dom";
 import Landing from './pages/Landing';
 import Admin from './pages/Admin';
 import Signup from './pages/Signup';
+import Reject from './pages/Reject';
 // ----------------------------------------------------------------------
 
 export default function Router() {
   return useRoutes([
     { path: '/', element: <Landing /> },
     { path: '/admin', element: <Admin /> },
-    { path: '/signup', element: <Signup /> }
+    { path: '/signup', element: <Signup /> },
+    { path: '/reject', element: <Reject /> }
   ]);
 }
