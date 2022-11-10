000f856a05f741d31c28668a0ea7c32b4829e09b
kadartibor <kadartibor24@gmail.com>
2022-11-10 06:46:27 +0100

    fixes routing on client side using hash route

--- frontend/secret-santa/src/index.js
+++ frontend/secret-santa/src/index.js
@@ -2,21 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 //import './style.css'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
     <ToastContainer />
-  </>
+  </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
